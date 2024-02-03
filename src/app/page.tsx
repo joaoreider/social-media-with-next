943681292438aaa694790a58bc9b9381b60cc5d8
Joao Paulo <joaopauloj405@gmail.com>
2024-02-02 21:09:31 -0300

    Refactor page and header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,3 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
-
 export default async function Home() {
-  const session = await auth();
-  return (
-    <div>
-      <form action={actions.signIn}>
-        <Button type="submit">Sign in with github</Button>
-      </form>
-      <form action={actions.signOut}>
-        <Button type="submit">Sign out</Button>
-      </form>
-      <Profile />
-    </div>
-  );
+  return <div>home Page</div>;
 }
